b941d65f248848111737b9de4928d0841e56ffec
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-05-02 21:54:29 +0530

    feat: add new todo

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -2,13 +2,14 @@ import { format } from "date-fns";
 
 export const TodoCard = ({ todoInfo }) => {
   const { id, task, isCompleted, createdAt } = todoInfo;
+
   return (
     <div className="todo-card">
       <label>
         <input type="checkbox" checked={isCompleted} />
         <span className={`task-completed-${isCompleted}`}>{task}</span>
       </label>
-      <small>{format(createdAt.toDate(), "MM/dd/yyyy")}</small>
+      <small>{format(new Date(createdAt), "dd/MM/yyyy")}</small>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { getDoc, doc } from "firebase/firestore";
+import { v4 as uuid } from "uuid";
+import { getDoc, doc, setDoc } from "firebase/firestore";
 import { ClipLoader } from "react-spinners";
 import toast from "react-hot-toast";
 import { Hero, Navbar, TodoCard } from "../components";
@@ -9,11 +10,11 @@ import { db } from "../firebase";
 export const Home = () => {
   const [todos, setTodos] = useState(null);
   const [showLoader, setShowLoader] = useState(false);
+  const [newTask, setNewTask] = useState("");
   const { user } = useAuth();
+  const documentRef = doc(db, "users", user.uid);
 
   useEffect(() => {
-    const documentRef = doc(db, "users", user.uid);
-
     (async () => {
       setShowLoader(true);
       try {
@@ -29,13 +30,47 @@ export const Home = () => {
         setShowLoader(false);
       }
     })();
-  }, [user]);
+  }, []);
+
+  const addNewTodo = async (e) => {
+    const newTodo = {
+      id: uuid(),
+      task: newTask,
+      isCompleted: false,
+      createdAt: new Date().getTime()
+    };
+    setShowLoader(true);
+    e.preventDefault();
+    try {
+      await setDoc(documentRef, {
+        todos: [...todos, newTodo]
+      });
+      setTodos((prevTodos) => prevTodos.concat(newTodo));
+      setNewTask("");
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setShowLoader(false);
+    }
+  };
 
   return (
     <div>
       <Navbar />
       <Hero />
       <ClipLoader loading={showLoader} speedMultiplier={2.5} size={20} />
+      <form onSubmit={addNewTodo}>
+        <input
+          onChange={(e) => setNewTask(e.target.value)}
+          type="text"
+          name="todo"
+          id="todo"
+          value={newTask}
+          placeholder="Enter new task"
+          required
+        />
+        <button className="btn">add</button>
+      </form>
       {todos &&
         todos.map((todoInfo) => (
           <TodoCard key={todoInfo.id} todoInfo={todoInfo} />
