92940ef3164f2309722ff04290c02adb68e42252
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-05-02 14:15:52 +0530

    refactor: change navbar parent

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import "./styles.css";
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Home, Login } from "./pages";
-import { RequireAuth, Navbar } from "./components";
+import { RequireAuth } from "./components";
 
 export default function App() {
   return (
     <div className="App">
       <Toaster position="bottom-center" />
-      <Navbar />
       <Routes>
         {/* Public Routes */}
         <Route path="/login" element={<Login />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
+import { Navbar } from "../components";
 import { useAuth } from "../contexts";
 
 export const Home = () => {
   const { user } = useAuth();
   return (
     <div>
+      <Navbar />
       <header className="hero">
         <p>Hellew {user?.email ?? "anonymous"}</p>
-        <img
-          className="hero-banner"
-          src="https://humornama.com/wp-content/uploads/2020/12/Toh-Kar-Na-meme-template-of-Arvind-Kejriwal.jpg"
-          alt="arvind-bhai-delhi-wale"
-        />
+        <div>
+          <img
+            className="hero-banner"
+            src="https://humornama.com/wp-content/uploads/2020/12/Toh-Kar-Na-meme-template-of-Arvind-Kejriwal.jpg"
+            alt="arvind-bhai-delhi-wale"
+          />
+        </div>
       </header>
     </div>
   );
